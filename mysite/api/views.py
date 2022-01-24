319430504100e519ead67a967b4771c9c16e96dd
KwabenaYeboah <kwabena4140@gmail.com>
2022-01-24 00:11:24 +0000

    Added view for Post and Users

--- mysite/api/views.py
+++ mysite/api/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from blog.models import Post
+from django.contrib.auth.models import User
+from serializers import PostSerializer, UserSerializer
+class PostView(viewsets.ModelViewsets):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class UserView(viewsets.ModelViewsets):
+    queryset  = User.objects.all()
+    serializer_class = UserSerializer
+    
