9ed374688fac7982ec0d9eff01e4187806b7f125
KwabenaYeboah <kwabena4140@gmail.com>
2022-01-25 08:15:04 +0000

    Added 'dj_rest_auth' for login logout endpoints

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     #3rd-party apps
     'rest_framework',
     'rest_framework.authtoken',
+    'dj_rest_auth',
 ]
 
 MIDDLEWARE = [
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -32,7 +32,8 @@ urlpatterns = [
     path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name='password_reset_complete'), 
     path('', include('blog.urls') ),
     path('api/v1/', include('api.urls')),
-    path('api-auth/', include('rest_framework.urls'))
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/v1/dj-rest-auth/', include('dj_rest_auth.urls')),
 ]
 
 if settings.DEBUG:  
