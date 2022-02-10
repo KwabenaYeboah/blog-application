21fa4be2fac7bf4e6a58f3f4a7a11d400fb56fd7
KwabenaYeboah <kwabena4140@gmail.com>
2022-02-10 19:16:03 +0000

    Added tests for signup templates

--- mysite/users/tests.py
+++ mysite/users/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class TestSignUpPage(TestCase):
+    def setUp(self):
+        self.response = self.client.get(reverse("register"))
+    
+    def test_signup_template(self):
+        self.assertEqual(self.response.status_code, 200)
+        self.assertTemplateUsed(self.response, "users/register.html")
+        self.assertContains(self.response, "Join Today")
+        self.assertNotContains(self.response, "This text is not there")
+        
